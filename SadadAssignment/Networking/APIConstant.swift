5e65ec20bf0b2bc2fc117afbc6415b248a514cc2
kiaelyac <kiarashelyac@gmail.com>
2024-08-12 19:38:27 +0330

    feature: implement api constant file

---
+++ SadadAssignment/Networking/APIConstant.swift
@@ -0,0 +1,18 @@
+//
+//  APIConstant.swift
+//  SadadAssignment
+//
+//  Created by kiarash on 8/12/24.
+//
+
+import Foundation
+
+/// A utility class that holds constant values for API configurations.
+/// This includes the base URL for the API and any common HTTP header fields or content types
+/// that might be needed across multiple API requests.
+final class ApiConstants {
+    
+    static var baseUrl = "https://c62881db-c803-4c5e-907e-3b1d843fa7fd.mock.pstmn.io/"
+    
+}
+
